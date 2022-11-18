f6ee40d0b5286508c18d66ff6d5cfb7151579fa8
warre333 <95884089+warre333@users.noreply.github.com>
2022-11-18 11:29:47 +0100

    Cleanup

--- client/src/config/api.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    API_URL: "http://localhost:4000/",
-    AUTH: "http://localhost:4000/auth/",
-    EMAIL: "http://localhost:4000/email/",
-    POSTS: "http://localhost:4000/posts/",
-    USERS: "http://localhost:4000/users/",
-
-    WEBSITE_URL: "http://localhost:3000/"
-}

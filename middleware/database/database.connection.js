0e829b0283b104c0f2b231fe21b8f66d1742047d
warre333 <95884089+warre333@users.noreply.github.com>
2023-05-26 14:02:31 +0200

    reverse

--- middleware/database/database.connection.js
+++ middleware/database/database.connection.js
@@ -2,19 +2,12 @@ const mysql = require("mysql");
 const dbConfig = require("../../config/database.config");
 require('dotenv').config()
 // Database connection
-const db = () => {
-  if ('DEBUG' in process.env) {
-    console.log("Env var is set:", process.env.DEBUG)
-  } else {
-    console.log("Env var IS NOT SET")
-  }
-}
-// mysql.createPool({
-//   host: process.env.HOST,
-//   user: process.env.USER,
-//   password: process.env.PASSWORD,
-//   database: process.env.DATABASE,
-//   charset: "utf8mb4",
-// });
+const db = mysql.createPool({
+  host: process.env.HOST,
+  user: process.env.USER,
+  password: process.env.PASSWORD,
+  database: process.env.DATABASE,
+  charset: "utf8mb4",
+});
 
 module.exports = db;

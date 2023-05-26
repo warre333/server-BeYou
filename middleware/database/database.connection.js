458ed8a76b27f925aae1a73fb487a5f23ddcea43
warre333 <95884089+warre333@users.noreply.github.com>
2023-05-26 14:00:36 +0200

    debug server env

--- middleware/database/database.connection.js
+++ middleware/database/database.connection.js
@@ -2,12 +2,19 @@ const mysql = require("mysql");
 const dbConfig = require("../../config/database.config");
 require('dotenv').config()
 // Database connection
-const db = mysql.createPool({
-  host: process.env.HOST,
-  user: process.env.USER,
-  password: process.env.PASSWORD,
-  database: process.env.DATABASE,
-  charset: "utf8mb4",
-});
+const db = () => {
+  if ('DEBUG' in process.env) {
+    console.log("Env var is set:", process.env.DEBUG)
+  } else {
+    console.log("Env var IS NOT SET")
+  }
+}
+// mysql.createPool({
+//   host: process.env.HOST,
+//   user: process.env.USER,
+//   password: process.env.PASSWORD,
+//   database: process.env.DATABASE,
+//   charset: "utf8mb4",
+// });
 
 module.exports = db;
